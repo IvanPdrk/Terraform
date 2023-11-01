f9d924f131356c03cc56a5f3382bf329b85d364e
Ivan Uribe <43795318+IvanPdrk@users.noreply.github.com>
2023-10-31 19:08:08 -0600

    Update main.tf

--- eks/main.tf
+++ eks/main.tf
@@ -8,18 +8,23 @@
 # }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks.cluster_name]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks.cluster_name]
 }
+data "aws_caller_identity" "current" {}
 
-# data "aws_availability_zones" "available" {
-# }
+data "aws_secretsmanager_secret" "argocd" {
+  name = "argocd.local.environment"
+}
 
+data "aws_secretsmanager_secret_version" "admin_password_version" {
+  secret_id = data.aws_secretsmanager_secret.argocd.id
+}
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -27,8 +32,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs                  = ["us-east-2a", "us-east-2b"]
-  private_subnets      = ["10.0.1.0/24","10.0.2.0/24"]
-  public_subnets      = ["10.0.3.0/24","10.0.4.0/24"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
   enable_dns_hostnames = true
   enable_nat_gateway   = true
   single_nat_gateway   = true
@@ -49,13 +54,34 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     to_port   = 443
     protocol  = "tcp"
     cidr_blocks = [
-      "10.0.0.0/16"
+      "10.0.0.0/16",
+      "172.20.0.0/16"
     ]
+  }
+}
 
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name_prefix      = "VPC-CNI-IRSA"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv6   = true
+  vpc_cni_enable_ipv4 = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
+    }
   }
 
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+
+  }
 }
- 
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -64,8 +90,10 @@ module "eks" {
   cluster_name    = var.cluster_name
   cluster_version = "1.27"
 
-  cluster_endpoint_public_access  = false
-  cluster_endpoint_private_access = true
+  create_iam_role = true
+
+  cluster_endpoint_public_access  = true
+  # cluster_endpoint_private_access = true
 
   cluster_addons = {
     coredns = {
@@ -75,14 +103,36 @@ module "eks" {
       most_recent = true
     }
     vpc-cni = {
-      most_recent = true
+      most_recent              = true
+      before_compute           = true
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
     }
 
   }
-  cluster_additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-  vpc_id                                = module.vpc.vpc_id
-  subnet_ids                            = module.vpc.private_subnets
-
+  
+  # cluster_additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+  create_cloudwatch_log_group = false
+  eks_managed_node_group_defaults = {
+    # ami_type       = "AL2_x86_64"
+    instance_types = ["t2.micro"]
+    create_iam_role = true
+        # We are using the IRSA created below for permissions
+    # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
+    # and then turn this off after the cluster/node group is created. Without this initial policy,
+    # the VPC CNI fails to assign IPs and nodes cannot join the cluster
+    # See https://github.com/aws/containers-roadmap/issues/1666 for more context
+    iam_role_attach_cni_policy = true
+  }
   eks_managed_node_groups = {
     one = {
       name          = "worker-group"
@@ -90,16 +140,22 @@ module "eks" {
       min_size      = 1
       desired_size  = 1
       instance_type = ["t2.micro"]
-
+      iam_role_attach_cni_policy = true
+      enable_bootstrap_user_data   = false
+      use_custom_launch_template = false
+      # create_launch_template = true
+      # subnet_ids = module.vpc.aws_subnets.public.ids
     }
   }
-
+  
 
   # aws-auth configmap
-  # create = true
-  create_aws_auth_configmap = false
+  create_aws_auth_configmap = true
   manage_aws_auth_configmap = true
 
+  aws_auth_accounts = [
+    "072204673534"
+  ]
   aws_auth_users = [
     {
       userarn  = "arn:aws:iam::072204673534:user/ivan-iam"
@@ -115,64 +171,64 @@ module "eks" {
 }
 
 
-resource "kubernetes_deployment" "example" {
-  metadata {
-    name = "terraform-example"
-    labels = {
-      test = "MyExampleApp"
-    }
-  }
+# resource "kubernetes_deployment" "example" {
+#   metadata {
+#     name = "terraform-example"
+#     labels = {
+#       test = "MyExampleApp"
+#     }
+#   }
 
-  spec {
-    replicas = 1
+#   spec {
+#     replicas = 1
 
-    selector {
-      match_labels = {
-        test = "MyExampleApp"
-      }
-    }
+#     selector {
+#       match_labels = {
+#         test = "MyExampleApp"
+#       }
+#     }
 
-    template {
-      metadata {
-        labels = {
-          test = "MyExampleApp"
-        }
-      }
+#     template {
+#       metadata {
+#         labels = {
+#           test = "MyExampleApp"
+#         }
+#       }
 
-      spec {
-        container {
-          image = "nginx:1.7.8"
-          name  = "example"
+#       spec {
+#         container {
+#           image = "nginx:1.7.8"
+#           name  = "example"
 
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#           resources {
+#             limits = {
+#               cpu    = "0.5"
+#               memory = "512Mi"
+#             }
+#             requests = {
+#               cpu    = "250m"
+#               memory = "50Mi"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
 
-resource "kubernetes_service" "example" {
-  metadata {
-    name = "terraform-example"
-  }
-  spec {
-    selector = {
-      test = "MyExampleApp"
-    }
-    port {
-      port        = 80
-      target_port = 80
-    }
+# resource "kubernetes_service" "example" {
+#   metadata {
+#     name = "terraform-example"
+#   }
+#   spec {
+#     selector = {
+#       test = "MyExampleApp"
+#     }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
 
-    type = "LoadBalancer"
-  }
-}
+#     type = "LoadBalancer"
+#   }
+# }
